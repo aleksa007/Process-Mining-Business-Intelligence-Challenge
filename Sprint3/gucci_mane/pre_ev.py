222c92aec3f4da2d060bd239003b9742cf284929
sakce_j <sakovic.fx@gmail.com>
2020-03-17 19:47:52 +0100

    New tool, 2017 combination algo use

---
+++ Sprint3/gucci_mane/pre_ev.py
@@ -0,0 +1,118 @@
+import argparse
+import pandas as pd
+import numpy as np
+from sklearn.metrics import mean_squared_error, accuracy_score
+
+'''For accuracy and rmse - do it for all three new columns'''
+def pre_road(train_path: str, test_path: str) -> object:
+    df2_test = pd.read_csv(train_path)
+    df2_train = pd.read_csv(test_path)
+
+    # Convert from string to datetime
+    df2_train['event time:timestamp'] = pd.to_datetime(df2_train['event time:timestamp'])
+    df2_test['event time:timestamp'] = pd.to_datetime(df2_test['event time:timestamp'])
+
+    # Concatenate the datasets
+    df2 = pd.concat([df2_train, df2_test])
+
+    # Sort the values and reset the index accordingly
+    df2 = df2.sort_values(by=['event time:timestamp'])
+    df2 = df2.reset_index(drop=True)
+
+    # Create dataframe with first events of each case in chronological order
+    df_cases_first_event = df2.drop_duplicates(subset='case concept:name', keep='first')
+    df_cases_first_event = df_cases_first_event.reset_index(drop=True)
+
+    # Split the cases in 80-20
+    index80 = int(len(df_cases_first_event) * 80 / 100 - 1)
+    df_cases_train = df_cases_first_event.loc[:index80]
+    df_cases_test = df_cases_first_event.loc[index80 + 1:]
+
+    # Training and test set complete
+    df_train_new = df2.loc[df2['case concept:name'].isin(list(df_cases_train['case concept:name']))]
+    df_test_new = df2.loc[df2['case concept:name'].isin(list(df_cases_test['case concept:name']))]
+
+    # Setting the 'now' (as the last event of the last case in the training set)
+    # now = df_train_new[df_train_new['case concept:name']==df_cases_train.iloc[-1]\
+    #    ['case concept:name']].iloc[-1]['event time:timestamp']
+
+    now = pd.Timestamp('2012-12-12')
+
+    # Training and test set before 'now'
+    df_train_new_now = df_train_new.loc[(df_train_new['event time:timestamp'] <= now)]
+    df_test_new_now = df_test_new.loc[(df_test_new['event time:timestamp'] <= now)]
+
+    # Add argument, if -p, output the preprocessed files as csv's
+    # df_test_new_now.to_csv('./data/road-test-pre.csv', index=False)
+    # df_train_new_now.to_csv('./data/road-train-pre.csv', index=False)
+
+    return df_train_new_now, df_test_new_now
+
+def pre_loan():
+    pass
+
+def run_pre():
+    '''Check which data is being used and call that function'''
+
+def evaluate_acc_rmse(base, perm, d_tree):
+
+    # Baseline Evaluation
+    event_real = np.array(base['Event_Name'])
+    event_real = event_real[1:]
+
+    time_real = np.array(base['Real_TimeDiff'])
+
+    event_pred_baseline = np.array(base['Baseline_Event'])[:-1]
+    # event_pred_baseline = event_pred_baseline[:-1]
+    time_pred_baseline = np.array(base['Baseline_TimeDiff'])
+
+    acc_baseline = accuracy_score(event_real, event_pred_baseline)
+    print('Accuracy for event prediction BASELINE: {}%'.format(round(acc_baseline, 2) * 100))
+    rms_baseline = np.sqrt(mean_squared_error(time_real, time_pred_baseline))
+    print('Root mean squared error for time difference prediction BASELINE: {}'.format(round(rms_baseline, 2)))
+
+    # Combinations Algorithm Evaluation
+    event_real = np.array(perm['Current_Event'])
+    event_real = event_real[1:]
+
+    event_pred_combs = np.array(perm['Combs_Event'])[:-1]
+    # event_pred_combs = event_pred_combs[:-1]
+    time_pred_combs = np.array(perm['Combs_TimeDiff'])
+
+    acc_combs = accuracy_score(event_real, event_pred_combs)
+    print('Accuracy for event prediction COMBINATIONS: {}%'.format(round(acc_combs, 2) * 100))
+
+    rms_combs = np.sqrt(mean_squared_error(time_real, time_pred_combs))
+    print('Root mean squared error for time difference prediction COMBINATIONS: {}'.format(round(rms_combs, 2)))
+
+    # Decision Tree Evaluation
+
+    event_real = np.array(d_tree['Next_Event'])  # taking next event col. as an array
+    event_pred = np.array(d_tree['DTree_Event'])  # taking the predictions as an array
+
+    acc_tree = accuracy_score(event_real, event_pred)  # calculates the accuracy based on the both arrays
+    print('Accuracy for event prediction DECISION TREE: {}%'.format(round(acc_tree, 2) * 100))
+
+    # RMSE
+
+    return print('Done.')
+def accuracy():
+    pass
+def rmse():
+    pass
+
+def run_ev():
+    accuracy(), rmse()
+
+if __name__ == '__main__':
+    #import sys
+    #if sys.argv[1] == 'p':
+    train, test = pre_road('./data/road-train.csv', './data/road-test.csv')
+    train.to_csv('./build/road-train-pre.csv')
+    test.to_csv('./build/road-test-pre.csv')
+
+    # elif sys.argv[1] == 'e':
+    #     run_ev()
+    # else:
+    #     run_ev()
+    #     run_pre()
---
+++ Sprint3/gucci_mane/readme.txt
@@ -0,0 +1,30 @@
+To run the tool do the following:
+
+1. Change the working directory into the /main directory and run pip install -r requirements.txt
+2. python gucci_mane.py ./data/road-train.csv ./data/road-test.csv ./predict.csv
+3. Wait for a few moments
+4. The new predict.csv file will show in the /main directory that includes predictions for both the new Decision tree algorith, 
+	the 'combinations' algorithm,
+	as well as for the baseline
+5. # TODO: Fix this -> When running the demo, after installing dependencies as in Step 1, run:
+	python main.py ./data/demo_train.csv ./data/demo_test.csv ./predict.csv --demo
+	-! In this case, the data is halved(both train and test) and output csv file only contains
+	predictions for the 'combinations' algorithm.
+
+
+
+At the moment, the tool has not been tested with other data. However, if you try to run it, then the script
+preprocessing is not going to run, because it was partly hard coded.
+
+Preprocessing of road data combines the data back again, and splits it in a way such that:
+- cases in train data finish by a certain 'now' moment
+- cases in test data start only after the 'now' moment
+- the 'now' moment is the hard-coded part, which is why this script is not run on other data
+
+This tool predicts the next event based on the combinations of previous two events, and predicts the most frequent event
+after that specific combination. Then, taking that prediction, it predicts the time it will take to get to it based on the 
+average time it takes for that specific combination. 
+
+Also, a decision tree is implemented as the third iteration of the model. 
+
+For a more detailed description inspect the Poster attached. 
